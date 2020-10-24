07bc9f8c65fbbfb76de0f6d1e91015f891b15b84
Alexander F. Bragin <alefbragin@gmail.com>
2020-10-24 09:25:16 +0800

    Implement routing sceleton

--- src/index.php
+++ src/index.php
@@ -1,5 +1,13 @@
 <?php
 
+$method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['REQUEST_URI'];
 
-echo "Under developing. Path is $path\n";
+if ($method == 'GET' && $path == '/v1/todos') {
+	echo "Todo list under developing.\n";
+} else if ($method == 'GET' && preg_match('|^/v1/todos/(\d{1,9})$|', $path, $matches)) {
+	$id = $matches[1];
+	echo "Todo item with ID = $id. Under developing.\n";
+} else {
+	echo "Not found.\n";
+}
