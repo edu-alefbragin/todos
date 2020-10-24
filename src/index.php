dd702c2b5314fba0a63e0f7cc87896795650621b
Alexander F. Bragin <alefbragin@gmail.com>
2020-10-24 10:05:01 +0800

    Return proper HTTP codes

--- src/index.php
+++ src/index.php
@@ -4,10 +4,10 @@ $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['REQUEST_URI'];
 
 if ($method == 'GET' && $path == '/v1/todos') {
-	echo "Todo list under developing.\n";
+	http_response_code(501);
 } else if ($method == 'GET' && preg_match('|^/v1/todos/(\d{1,9})$|', $path, $matches)) {
 	$id = $matches[1];
-	echo "Todo item with ID = $id. Under developing.\n";
+	http_response_code(501);
 } else {
-	echo "Not found.\n";
+	http_response_code(404);
 }
